66831c5ef34c655e77dc7c36a158743eb48eda5c
sensible-proc-7557 <vchan313@gmail.com>
2018-11-12 21:31:20 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+		animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
+  end	 
